46b6a5197f7607bb25c276eb5c0168e190865c33
Zoe Ang <zwAng@mcm.edu.ph>
2025-01-27 13:20:59 +0800

    Add files via upload

---
+++ DataTypesExample.java
@@ -0,0 +1,11 @@
+public class DataTypesExample {
+    public static void main(String[] args) {
+    int age = 18;
+    double height = 1.5;
+    boolean isStudent = true;
+    String name = "Zoe Wednesday L. Ang";
+
+    System.out.println("Hi, my name is " + name + ". I am " + age + " years old, " + height + " meters tall, and it is " + isStudent + " that I am a student.");
+    
+    }
+}
